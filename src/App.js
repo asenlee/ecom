fb5f308a001b5221aae56f81a4a86f02b073e4e1
asenlee <woochang.lee0405@gmail.com>
2023-08-21 22:35:01 -0700

    add homescreen and productscreen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div>
-      My App
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+            <Routes>
+              <Route path="/" Component={HomeScreen} exact />
+              <Route path="/product/:id" Component={ProductScreen} />
+            </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/products.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+        'id': '1',
+        'name': 'T-shirt 1',
+        'image': '/images/shirt1.jpg',
+        'description': 'T-shirt 1 description',
+        'category': 'T-shirt',
+        'price': 29.99,
+        'countInStock': 10,
+        'rating': 4.8,
+        'numReviews': 19,
+    },
+    {
+        'id': '2',
+        'name': 'T-shirt 2',
+        'image': '/images/shirt2.jpg',
+        'description': 'T-shirt 2 description',
+        'category': 'T-shirt',
+        'price': 39.99,
+        'countInStock': 8,
+        'rating': 4.7,
+        'numReviews': 12,
+    },
+    {
+        'id': '3',
+        'name': 'T-shirt 3',
+        'image': '/images/shirt3.jpg',
+        'description': 'T-shirt 3 description',
+        'category': 'T-shirt',
+        'price': 34.99,
+        'countInStock': 0,
+        'rating': 4.9,
+        'numReviews': 14,
+    },
+]
+
+export default products
